d86562cc98e5605e813d1a86659b3a34452031d8
lopesgeneses <geneses.lopes@ext.webedia-group.com>
2021-08-13 14:37:28 -0300

    criando ambiente dev

---
+++ .docker/app/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Backend
+# cd backend
+
+# if [ ! -f ".env" ]; then
+#     cp .env.example .env
+# fi
+
+# if [ ! -f ".env.testing" ]; then
+#     cp .env.testing.example .env.testing
+# fi
+
+# composer install
+
+php-fpm
