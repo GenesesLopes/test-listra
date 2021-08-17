fa7f1721007ad2b3ae1a60d5f056e44cfb9aa6bc
lopesgeneses <geneses.lopes@ext.webedia-group.com>
2021-08-17 01:51:17 +0000

    Criando tela e eventos

--- frontend/src/components/Form/index.js
+++ frontend/src/components/Form/index.js
@@ -1,3 +1,83 @@
+import { BIcon } from 'bootstrap-vue'
+import { VMoney } from 'v-money'
+import parsePrice from '../../helpers/parsePrice';
+import financingCalculation from '../../utils/financingCalculation';
 export default {
-    name: 'Form'
+    name: 'Form',
+    components: {
+        BIcon
+    },
+    data() {
+        return {
+            selected: null,
+            dataVehicles: [],
+            calculateFinancing: [],
+            initialValue: 0.00,
+            money: {
+                decimal: ',',
+                thousands: '.',
+                prefix: 'R$ ',
+                precision: 2
+            }
+        }
+    },
+    directives: { money: VMoney },
+    watch: {
+        selected: function (data) {
+            if (data === null){
+                this.initialValue = 0.00;
+                this.calculateFinancing = [];
+            }
+        }
+    },
+    methods: {
+        async getData() {
+            try {
+                const restData = await new Promise((resolve) => {
+                    setTimeout(() => {
+                        resolve(
+                            [
+                                { id: 'a', description: 'Carro 01', price: 100000.35 },
+                                { id: 'b', description: 'Carro 02', price: 120.30 },
+                                { id: 'c', description: 'Carro 03', price: 140.98 },
+                            ]
+                        )
+                    }, 2000)
+                });
+                this.dataVehicles = [...this.dataVehicles, ...restData];
+            } catch (error) {
+                console.error(error)
+            }
+
+        },
+        parseInitalValue(initialValue) {
+            return parseFloat(
+                initialValue.replace("R$", "")
+                    .replaceAll('.', '')
+                    .replaceAll(',', '.')
+            );
+        },
+        handleCalculate() {
+            let valueParse = this.parseInitalValue(this.initialValue)
+            
+            this.calculateFinancing = [];
+            this.calculateFinancing = financingCalculation(valueParse, this.selected.price)
+            console.log(
+                // this.selected,
+                valueParse,
+                this.selected.price,
+                this.calculateFinancing
+            )
+            
+        }
+    },
+    filters: {
+        parsePriceValue(price){
+            return parsePrice(price)
+        }
+    },
+    async mounted() {
+        this.dataVehicles = [];
+        await this.getData();
+    },
 }
--- frontend/src/components/Form/index.vue
+++ frontend/src/components/Form/index.vue
@@ -1,4 +1,77 @@
 <template>
-    <p>Formulário</p>
+    <b-container class="mt-2">
+        <b-card header-tag="header">
+            <template #header>
+                <h6 class="mb-0">Meus Veículos</h6>
+            </template>
+            <b-form @submit.prevent="handleCalculate">
+                <b-form-group>
+                    <v-select
+                        v-model="selected"
+                        :options="dataVehicles"
+                        label="description"
+                        placeholder="Selecione um veículo"
+                    />
+                </b-form-group>
+                <b-container v-if="selected != null">
+                    <b-form-row>
+                        <b-col>
+                            <p>
+                                <b-icon icon="chevron-right" />
+                                {{ selected.description }}
+                            </p>
+                            <p>
+                                <b-icon icon="chevron-right" />
+                                <strong
+                                    >{{ selected.price | parsePriceValue }}
+                                </strong>
+                            </p>
+                        </b-col>
+                    </b-form-row>
+                    <b-form-row>
+                        <b-col sm="10">
+                            <b-form-group
+                                label="Simulação de financiamento"
+                                label-for="initialValue"
+                            >
+                                <b-form-input
+                                    id="initialValue"
+                                    v-model.lazy="initialValue"
+                                    v-money="money"
+                                    trim
+                                ></b-form-input>
+                            </b-form-group>
+                        </b-col>
+                        <b-col sm="2">
+                            <b-form-group>
+                                <b-button 
+                                    variant="success" 
+                                    class="align-button"
+                                    type="submit"
+                                >
+                                    Simular
+                                </b-button>
+                            </b-form-group>
+                        </b-col>
+                    </b-form-row>
+                    <b-row v-if="calculateFinancing.length">
+                        <b-col>
+                            <p class="h6">Valores simulados para você</p>
+                            <div v-for="(dataFinancing, index ) in calculateFinancing" :key="index">
+                                <span>{{dataFinancing.installment + 'x de ' + dataFinancing.value}}</span>
+                            </div>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </b-form>
+        </b-card>
+    </b-container>
 </template>
+<style>
+@media (min-width: 576px) {
+    .align-button {
+        margin-top: 32px;
+    }
+}
+</style>
 <script src="./index.js" />
