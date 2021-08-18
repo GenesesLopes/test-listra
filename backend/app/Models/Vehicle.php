f3aa17f74a7398dacd54aac32ab5fa3701fb4097
lopesgeneses <geneses.lopes@ext.webedia-group.com>
2021-08-18 00:23:42 +0000

    Configurando Model

--- backend/app/Models/Vehicle.php
+++ backend/app/Models/Vehicle.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Vehicle extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['id','description','price'];
+
+    protected $casts = [
+        'price'=> 'float'
+    ];
+
 }
