85cae1f7a58e994190f0b56cdb3a6eb6e9eae054
lopesgeneses <geneses.lopes@ext.webedia-group.com>
2021-08-18 02:10:01 +0000

    Regra de negocio

--- backend/app/Http/Controllers/VehicleController.php
+++ backend/app/Http/Controllers/VehicleController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\VehicleRequest;
 use App\Models\Vehicle;
 use Illuminate\Http\Request;
 
 class VehicleController extends Controller
 {
-    public function index()
+    public function index(VehicleRequest $request)
     {
-        //
+        $per_page  = $request->input('per_page',10);
+        $current_page = $request->input('current_page', 1);
+        $search = $request->input('search', '');
+        
+        if($search !== ''){
+            return Vehicle::where('description','like',"%{$search}%")
+                ->paginate(
+                    $per_page,
+                    page:$current_page
+                );
+        }
+        return Vehicle::paginate(
+            $per_page,
+            page:$current_page
+        );
     }
     
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['prefix' => 'v1'],function() {
-    Route::get('/vehicles');
+    Route::get('/vehicles',[VehicleController::class,'index']);
 });
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
