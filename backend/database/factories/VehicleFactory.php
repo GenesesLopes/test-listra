286d1fd276924af64ef14a808a60a7bbd714b21c
lopesgeneses <geneses.lopes@ext.webedia-group.com>
2021-08-18 15:28:29 +0000

    Aumentando numero de veículos cadastrados

--- backend/database/factories/VehicleFactory.php
+++ backend/database/factories/VehicleFactory.php
@@ -7,6 +7,41 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class VehicleFactory extends Factory
 {
+
+    /** @var array */
+    const CARS = [
+        'Fiat Strada',
+        'Fiat Argo',
+        'Hyundai HB20',
+        'Fiat Mobi',
+        'Jeep Renegade',
+        'Renault Kwid',
+        'Jeep Compass',
+        'Fiat Toro',
+        'Hyundai Creta',
+        'Fiat Cronos',
+        'VW Gol',
+        'Toyota Corolla Cross',
+        'Toyota Corolla',
+        'Toyota Hilux',
+        'Chevrolet S10',
+        'VW T-Cross',
+        'Nissan Kicks',
+        'VW Saveiro',
+        'VW Nivus',
+        'Fiat Uno',
+        'Hyundai HB20S',
+        'Chevrolet Onix',
+        'Honda HR-V',
+        'Renault Duster',
+        'Citroën C4 Cactus',
+        'Toyota Yaris Hatch',
+        'Chevrolet Spin',
+        'VW Virtus',
+        'Honda Civic',
+        'Fiat Fiorino'
+    ];
+
     /**
      * The name of the factory's corresponding model.
      *
@@ -31,8 +66,9 @@ class VehicleFactory extends Factory
      */
     public function definition()
     {
+        
         return [
-            'description' => $this->faker->colorName() .' '. $this->faker->colorName(),
+            'description' => self::CARS[array_rand(self::CARS)] .' '. $this->faker->colorName(),
             'price' => $this->faker->randomFloat(2,max: 1000000)
         ];
     }
